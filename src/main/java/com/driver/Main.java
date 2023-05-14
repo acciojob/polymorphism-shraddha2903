0b4c6f20dbe53db58384531f21cca2a9be4de580
shraddha2903 <70905602+shraddha2903@users.noreply.github.com>
2023-05-14 12:53:53 +0530

    Task 2 completed, creating objecct of product in main

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,11 +1,12 @@
 package com.driver;
 
 public class Main {
-    class product{
+    public static class product{
 
     }
     public static void main(String args[])
     {
+        product p= new product();
 
     }
 
