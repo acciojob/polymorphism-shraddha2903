f446a586bb10bf0511359fd419ba9e4fd069a1f2
shraddha2903 <70905602+shraddha2903@users.noreply.github.com>
2023-05-14 13:16:22 +0530

    final commit again..

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,26 +1,28 @@
 package com.driver;
 
-public class Main {
-     static class Product{
+class Product{
 
-        public int product(int x,int y)
-        {
-            return x*y;
-        }
-        public int product(int x,int y,int z){
-            return x*y*z;
-        }
-        public double product(double x,double y){
-            return (double)x*y;
-        }
-
+    public int product(int x,int y)
+    {
+        return x*y;
     }
+    public int product(int x,int y,int z)
+    {
+        return x*y*z;
+    }
+    public double product(double x,double y)
+    {
+        return x*y;
+    }
+}
+public class Main {
+
     public static void main(String args[])
     {
         Product p = new Product();
         System.out.println(p.product(2,5));
         System.out.println(p.product(2,4,3));
-        System.out.println(p.product(1.2,3.5));
+        System.out.println(p.product(1.00,3.00));
 
     }
 
