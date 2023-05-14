040f1f9e7242c0c316ab3733b5cb4685a9f2427f
shraddha2903 <70905602+shraddha2903@users.noreply.github.com>
2023-05-14 13:31:05 +0530

    creating Product class outside the Main class

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,20 +1,6 @@
 package com.driver;
 
-class Product{
 
-    public int product(int x,int y)
-    {
-        return x*y;
-    }
-    public int product(int x,int y,int z)
-    {
-        return x*y*z;
-    }
-    public double product(double x,double y)
-    {
-        return x*y;
-    }
-}
 public class Main {
 
     public static void main(String args[])
