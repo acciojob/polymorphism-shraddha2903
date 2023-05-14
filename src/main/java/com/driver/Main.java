5ccdf9962080315d612baf3087a7341ea7e04a39
shraddha2903 <70905602+shraddha2903@users.noreply.github.com>
2023-05-14 12:55:58 +0530

    Task 3 completed creating product(x,y) method in product

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -3,10 +3,16 @@ package com.driver;
 public class Main {
     public static class product{
 
+        public int product(int x,int y)
+        {
+            return x*y;
+        }
+
     }
     public static void main(String args[])
     {
         product p= new product();
+        System.out.println(p.product(2,5));
 
     }
 
