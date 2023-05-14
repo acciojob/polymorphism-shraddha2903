0908f125e3dfce0d9b4c5974e712907a94c0c2b1
shraddha2903 <70905602+shraddha2903@users.noreply.github.com>
2023-05-14 12:57:18 +0530

    Task 4 completed creating product(x,y,z) method in product

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -7,12 +7,16 @@ public class Main {
         {
             return x*y;
         }
+        public int product(int x,int y,int z){
+            return x*y*z;
+        }
 
     }
     public static void main(String args[])
     {
         product p= new product();
         System.out.println(p.product(2,5));
+        System.out.println(p.product(2,4,3));
 
     }
 
